052a13bd1d9eb2e74f63a56e7cd70968561e8c04
Nabil Djaber <40780645+djbankrol@users.noreply.github.com>
2021-09-07 09:12:22 -0600

    Update NumbersRiddle.java

--- java/NumbersRiddle.java
+++ java/NumbersRiddle.java
@@ -1,7 +1,87 @@
 public class NumbersRiddle {
     public static void main(String[] args) {
-       double input;
-       System.out.println("Variable inputted into the ridde: " + input);
-        
+        int positiveInteger = 3;
+        int negativeInteger = -3;
+        int zero = 0;
+        int one = 1;
+        double positiveDouble = 3.5;
+        double negativeDouble = -3.5;
+
+        System.out.println("Variable inputted into the ridde: " + positiveInteger);
+        // Initialize variable to use calculations for
+        double inputAlgorithm = positiveInteger*2;
+        System.out.println("Variable multiplied by 2: " + inputAlgorithm);
+        inputAlgorithm += 6;
+        System.out.println(" Variable multiplied by 2 plus 6: " + inputAlgorithm);
+        inputAlgorithm /= 2;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2: " + inputAlgorithm);
+        inputAlgorithm -= positiveInteger;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2 subratcted by original value: " + inputAlgorithm);
+
+        System.out.println(" ");
+
+        System.out.println("Variable inputted into the ridde: " + negativeInteger);
+        // Initialize variable to use calculations for
+        inputAlgorithm = negativeInteger*2;
+        System.out.println("Variable multiplied by 2: " + inputAlgorithm);
+        inputAlgorithm += 6;
+        System.out.println(" Variable multiplied by 2 plus 6: " + inputAlgorithm);
+        inputAlgorithm /= 2;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2: " + inputAlgorithm);
+        inputAlgorithm -= negativeInteger;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2 subratcted by original value: " + inputAlgorithm);
+
+        System.out.println(" ");
+
+        System.out.println("Variable inputted into the ridde: " + zero);
+        // Initialize variable to use calculations for
+        inputAlgorithm = zero*2;
+        System.out.println("Variable multiplied by 2: " + inputAlgorithm);
+        inputAlgorithm += 6;
+        System.out.println(" Variable multiplied by 2 plus 6: " + inputAlgorithm);
+        inputAlgorithm /= 2;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2: " + inputAlgorithm);
+        inputAlgorithm -= zero;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2 subratcted by original value: " + inputAlgorithm);
+
+        System.out.println(" ");
+
+        System.out.println("Variable inputted into the ridde: " + one);
+        // Initialize variable to use calculations for
+        inputAlgorithm = one*2;
+        System.out.println("Variable multiplied by 2: " + inputAlgorithm);
+        inputAlgorithm += 6;
+        System.out.println(" Variable multiplied by 2 plus 6: " + inputAlgorithm);
+        inputAlgorithm /= 2;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2: " + inputAlgorithm);
+        inputAlgorithm -= one;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2 subratcted by original value: " + inputAlgorithm);
+
+        System.out.println(" ");
+
+        System.out.println("Variable inputted into the ridde: " + positiveDouble);
+        // Initialize variable to use calculations for
+        inputAlgorithm = positiveDouble*2;
+        System.out.println("Variable multiplied by 2: " + inputAlgorithm);
+        inputAlgorithm += 6;
+        System.out.println(" Variable multiplied by 2 plus 6: " + inputAlgorithm);
+        inputAlgorithm /= 2;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2: " + inputAlgorithm);
+        inputAlgorithm -= positiveDouble;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2 subratcted by original value: " + inputAlgorithm);
+
+        System.out.println(" ");
+
+        System.out.println("Variable inputted into the ridde: " + negativeDouble);
+        // Initialize variable to use calculations for
+        inputAlgorithm = negativeDouble*2;
+        System.out.println("Variable multiplied by 2: " + inputAlgorithm);
+        inputAlgorithm += 6;
+        System.out.println(" Variable multiplied by 2 plus 6: " + inputAlgorithm);
+        inputAlgorithm /= 2;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2: " + inputAlgorithm);
+        inputAlgorithm -= negativeDouble;
+        System.out.println("Variable multiplied by 2 plus 6 divided by 2 subratcted by original value: " + inputAlgorithm);
+
     }
 }
